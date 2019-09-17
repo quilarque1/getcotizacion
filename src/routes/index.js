e2264b18171a926ba5ca3072feb6c21aa3598c06
Fquilar <quilarque1@gmail.com>
2019-09-16 22:34:59 -0300

    Primer test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.json({"Title": "Hello World"});
+router.get('/cotizacion', (req, res) => {
+    
+    res.json({ "title": "Hello World" });
+        
 });
 
 module.exports = router;
---
+++ test/app_test.js
@@ -0,0 +1,70 @@
+const chai = require("chai");
+const chaihttp = require("chai-http");
+const app = require("../src/routes/index");
+const expect = require('chai').expect;
+const fetch = require('node-fetch');
+
+var supertest = require("supertest");
+var should = require("should");
+
+
+chai.use(chaihttp);
+chai.should();
+const server = supertest.agent('http://localhost:3000');
+
+/* describe('API REST', function () {
+    it('GET /users debe devolver todos los usuarios', function (donde) {
+        server
+            .get("/cotizacion")
+            .expect("Content-type", /json/)
+            .expect(200)
+            .end(function (err, res) {
+                // HTTP status should be 200
+                res.status.should.equal(200);
+                // Error key should be false.
+                res.body.error.should.equal(false);
+                done();
+            });
+
+    });
+});
+
+describe('get all countries: ', () => {
+
+    it('should get all countries', (done) => {
+        chai.request(server)
+            .get('/cotizacion')
+            .end(function (err, res) {
+                console.log(res.body)
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
+
+});
+
+describe('/GET book', () => {
+    it('it should GET all the books', (done) => {
+        chai.request(server)
+            .get('/cotizacion')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(0);
+                done();
+            });
+    });
+});
+ */
+
+describe('API REST', function () { 
+    it('GET /cotizacion', async () => {
+
+        const response = await fetch('http://localhost:3000/cotizacion');
+        expect(response.status).to.be.equal(200);
+
+        const cotiz = await response.json();
+        //expect(cotiz.body).equal({ "title": "Hello World"});
+       
+    });
+});
